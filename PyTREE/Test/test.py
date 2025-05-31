13fafe159973d247b63d63786706c6cb077f8590
Mitchell Talyat <49891937+mtalyat@users.noreply.github.com>
2025-05-31 09:17:29 -0500

    Update test.py

--- PyTREE/Test/test.py
+++ PyTREE/Test/test.py
@@ -1,9 +1,6 @@
 import os
 import sys
 
-import PyTREE.Source
-import PyTREE.Source.setup
-
 BUILD_DIR = os.path.join(os.path.dirname(__file__), '..', 'Source', 'Build', 'build', 'lib.win-amd64-cpython-313')
 sys.path.append(BUILD_DIR)
 
@@ -12,5 +9,10 @@ import PyTREE
 PyTREE.init()
 
 print("PyTREE initialized successfully.")
+print(f"Time: {PyTREE.time_now()}")
+i = input("Input something: ")
+print(f"You entered: {i}")
+print(f"Time: {PyTREE.time_now()}")
+
 
 PyTREE.deinit()
